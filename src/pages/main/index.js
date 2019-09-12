dc6bd047d7618c6c047b0211ec5e13ca9944ae3e
GeovaneGraef <geovane.graef@hotmail.com>
2019-09-12 01:50:48 -0300

    Primeiro Commit

---
+++ src/pages/main/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import './style.css';
+
+export default class Main extends Component {
+    state = {
+        products: []
+    };
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get("/products");
+
+        this.setState({ products: response.data.docs });
+    };
+
+    render() {
+        const { products } = this.state;
+
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article>
+                        <strong>{product._id}</strong>
+                        <p>{product.description}</p>
+
+                        <a href="#">Acessar</a>
+                    </article>
+                ))}
+            </div>
+        );
+    }
+}
